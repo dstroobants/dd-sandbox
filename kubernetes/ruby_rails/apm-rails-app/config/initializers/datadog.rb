0eb971e6b492d0432fc8173a7e00b41474eeaf51
dstroobants <denistroobants@gmail.com>
2025-09-15 11:01:35 +0100

    Adding profiling and flat sample rate in code

--- kubernetes/ruby_rails/apm-rails-app/config/initializers/datadog.rb
+++ kubernetes/ruby_rails/apm-rails-app/config/initializers/datadog.rb
@@ -1,4 +1,11 @@
 Datadog.configure do |c|
-  # Add additional configuration here.
-  # Activate integrations, change tracer settings, etc...
+  c.tracing.instrument :sidekiq, quantize: { args: { show: :all } }
+  c.dynamic_instrumentation.enabled = true
+  c.profiling.enabled = true
+
+  c.tracing.sampling.rules = '[
+    {"service": "minelist-sidekiq", "resource": "sidekiq.*", "sample_rate": 0.01},
+    {"service": "minelist-sidekiq", "sample_rate": 1.0},
+    {"sample_rate": 1.0}
+  ]'
 end
