ffe0696ca571189ee9b1a394beb3ba03aa53c1d2
dstroobants <denistroobants@gmail.com>
2026-02-05 13:17:01 +0000

    custom ddtrace wheel

--- kubernetes/ruby_rails/apm-rails-app/config/initializers/datadog.rb
+++ kubernetes/ruby_rails/apm-rails-app/config/initializers/datadog.rb
@@ -8,4 +8,15 @@ Datadog.configure do |c|
     {"service": "minelist-sidekiq", "sample_rate": 1.0},
     {"sample_rate": 1.0}
   ]'
+  
+  #agent_settings = Datadog::Core::Configuration::AgentSettingsResolver.call(c)
+  #
+  #c.tracing.writer = Datadog::Tracing::SyncWriter.new(
+  #  agent_settings: agent_settings,
+  #  transport: Datadog::Tracing::Transport::HTTP.default(
+  #    agent_settings: agent_settings
+  #  ) do |t|
+  #    t.adapter :test  # No-op adapter, doesn't send to agent
+  #  end
+  #)
 end
